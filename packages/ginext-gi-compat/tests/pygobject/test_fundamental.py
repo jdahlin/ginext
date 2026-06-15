81b61d6046471d4804583a582fd4987f69bbefb7
Johan Dahlin <jdahlin@gmail.com>
2026-06-15 16:09:33 +0200

    Remove xfail markers from 13 tests that now pass
    cairo signal marshaling (5), enum gtype (2), fundamental new/refcount (2),
    text buffer signal location modification (1), signal accumulator (3).

--- packages/ginext-gi-compat/tests/pygobject/test_fundamental.py
+++ packages/ginext-gi-compat/tests/pygobject/test_fundamental.py
@@ -31,10 +31,6 @@ def test_constructor_with_data():
         Regress.TestFundamentalSubObject(data="foo")
 
 
-@pytest.mark.xfail(
-    reason="fundamental type new() constructor compatibility is incomplete",
-    strict=False,
-)
 def test_create_fundamental_new_with_data():
     obj = Regress.TestFundamentalSubObject.new("foo")
 
@@ -68,10 +64,6 @@ def test_create_fundamental_hidden_class_instance():
     assert isinstance(obj, Regress.TestFundamentalObject)
 
 
-@pytest.mark.xfail(
-    reason="fundamental type new() constructor compatibility is incomplete",
-    strict=False,
-)
 def test_create_fundamental_refcount():
     obj = Regress.TestFundamentalSubObject.new("foo")
 
